f6c3626d44416ee01c68d0ded8731994df5551c7
Khadija ABDERRAHIM <khadija.abderrahim1991@gmail.com>
2025-01-17 12:12:17 +0100

    1 [FE] Create front-end empty application

--- it-vibe-fe/src/App.vue
+++ it-vibe-fe/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <h1>it-vibe-fe</h1>
+    <nav class="navbar">
+      <ul class="menu">
+        <li class="logo">IT Vibe</li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="search-container">
+      <input type="text" placeholder="Search..." class="search-bar" />
+    </div>
   </div>
 </template>
 
@@ -11,12 +22,63 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  color: #333;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.navbar {
+  background-color: #333;
+  overflow: hidden;
+}
+
+.menu {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
+
+.menu li {
+  padding: 14px 20px;
+}
+
+.menu li a {
+  color: white;
+  text-decoration: none;
+  display: block;
+}
+
+.menu li a:hover {
+  background-color: #575757;
+}
+
+.menu li.logo {
+  flex-grow: 1;
+  text-align: left;
+  padding-left: 20px;
+  color: white;
+}
+
+.search-container {
+  margin-top: 50px;
+}
+
+.search-bar {
+  padding: 10px;
+  width: 50%;
+  max-width: 500px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.search-bar:focus {
+  outline: none;
+  border-color: #333;
 }
 </style>
