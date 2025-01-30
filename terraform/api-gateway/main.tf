932585dc5101b47aa43788b2824df147422de393
Zied AYADI <zied.ayedi22@gmail.com>
2025-01-30 16:59:23 +0100

    #26 [BE] Create aws api gateway and return harcoded data

---
+++ terraform/api-gateway/main.tf
@@ -0,0 +1,96 @@
+resource "aws_api_gateway_rest_api" "itvibe_api" {
+  name        = "ItVibeAPI"
+  description = "API for IT-Vibe"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "companies" {
+  rest_api_id = aws_api_gateway_rest_api.itvibe_api.id
+  parent_id   = aws_api_gateway_rest_api.itvibe_api.root_resource_id
+  path_part   = "companies"
+}
+
+resource "aws_api_gateway_method" "get_companies" {
+  rest_api_id   = aws_api_gateway_rest_api.itvibe_api.id
+  resource_id   = aws_api_gateway_resource.companies.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_companies_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.itvibe_api.id
+  resource_id             = aws_api_gateway_resource.companies.id
+  http_method             = aws_api_gateway_method.get_companies.http_method
+  integration_http_method = "GET"
+  type                    = "MOCK"
+  request_templates = {
+    "application/json" = jsonencode(
+      {
+        statusCode = 200
+      }
+    )
+  }
+}
+
+resource "aws_api_gateway_method_response" "get_companies_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.itvibe_api.id
+  resource_id = aws_api_gateway_resource.companies.id
+  http_method = aws_api_gateway_method.get_companies.http_method
+  response_models = {
+    "application/json" = "Empty"
+  }
+  status_code = 200
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+    rest_api_id = aws_api_gateway_rest_api.itvibe_api.id
+    resource_id = aws_api_gateway_resource.companies.id
+    http_method = aws_api_gateway_method.get_companies.http_method
+    status_code = aws_api_gateway_method_response.get_companies_method_response.status_code
+    response_templates = {
+        "application/json" = <<EOF
+        {
+            [
+  {
+    "name": "Capgemeni",
+    "location": "Pune",
+    "code": "C1",
+    "size": "1000"
+  },
+  {
+    "name": "TCS",
+    "location": "Mumbai",
+    "code": "T1",
+    "size": "2000"
+  },
+  {
+    "name": "Infosys",
+    "location": "Bangalore",
+    "code": "I1",
+    "size": "3000"
+  },
+  {
+    "name": "Wipro",
+    "location": "Chennai",
+    "code": "W1",
+    "size": "4000"
+  }
+]
+
+        }
+        EOF
+    }
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.get_companies_integration]
+  rest_api_id = aws_api_gateway_rest_api.itvibe_api.id
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  stage_name = "dev"
+  rest_api_id = aws_api_gateway_rest_api.itvibe_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+}
---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "it-vibe-terraform-backend-state"
+    key            = "it-vibe/terraform.tfstate"
+    region         = "eu-west-3" 
+    encrypt        = true
+  }
+}
+
+module "api-gateway" {
+  source = "./api-gateway"
+}
