174665edb93d110dffc27cab8db80e6bad244710
Zied AYADI <zied.ayedi22@gmail.com>
2025-01-30 22:40:37 +0100

    Issue #31 Create a lambda that return hardcoded compagnies

---
+++ terraform/lambda/main.tf
@@ -0,0 +1,34 @@
+variable "code_path" {
+  default = "../it-vibe-be/lambda/get-companies/get_companies.zip"
+}
+
+resource "aws_lambda_function" "get_companies_lambda" {
+    filename         = var.code_path
+    function_name    = "get_companies"
+    role             = aws_iam_role.lambda_exec.arn
+    handler          = "get_companies.lambda_handler"
+    runtime          = "python3.8"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+    name = "lambda_exec_role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Sid    = ""
+                Principal = {
+                    Service = "lambda.amazonaws.com"
+                }
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+    role       = aws_iam_role.lambda_exec.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
