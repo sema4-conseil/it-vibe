f89afadcbb7d15cefa7d7e4ec6f5c4b509759daf
Zied AYADI <zied.ayedi22@gmail.com>
2025-03-20 14:26:06 +0100

    Issue #54: Contact page ApiGateway
    - Add OpenApi Gateway declaration for the new contact endoint.

--- terraform/api-gateway/variables.tf
+++ terraform/api-gateway/variables.tf
@@ -3,5 +3,8 @@ variable "get_companies_lambda_arn" {}
 
 variable "save_company_lambda_invoke_arn" {}
 variable "save_company_lambda_arn" {}
+variable "push_contact_message_lambda_arn" {}
+variable "push_contact_message_lambda_invoke_arn" {}
+  
 
 variable "openapi_spec_location"  {}
