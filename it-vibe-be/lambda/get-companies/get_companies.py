ed358781f79db91ef349ebd5ae173c36a9dc6623
Zied AYADI <zied.ayedi22@gmail.com>
2025-03-26 16:04:10 +0100

    Issue #77: Home page paginated result
    - Update Lambda get_companies to return paginated Result

--- it-vibe-be/lambda/get-companies/get_companies.py
+++ it-vibe-be/lambda/get-companies/get_companies.py
@@ -2,17 +2,43 @@ import json
 import boto3
 import os
 
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['COMPANIES_TABLE_NAME']) # Get table name from environment variable
+
+
+
 def lambda_handler(event, context):
     try:
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table(os.environ['COMPANIES_TABLE_NAME']) # Get table name from environment variable
+        startKey = None
+        pageSize = 5  # Default page size
 
-        response = table.scan() # Scan the entire table. For large tables, use pagination.
-        items = response.get('Items', [])
+        if event and 'queryStringParameters' in event and event['queryStringParameters']: # check if event exist and if query string parameters exist
+            startKey = event['queryStringParameters'].get('startKey')
+            pageSize = int(event['queryStringParameters'].get('pageSize', 5))
+
+        query_kwargs = {
+            'Limit': pageSize
+        }
+
+        # If start key is there, add it to query kwargs. 
+        if startKey:
+            query_kwargs['ExclusiveStartKey'] = json.loads(startKey)
+
+       
+        result = table.scan(**query_kwargs) # Scan the entire table. For large tables, use pagination.
+        items = result.get('Items', [])
+
+        response = {
+            "items": items,
+            "LastEvaluatedKey": result.get('LastEvaluatedKey', None),
+            "ScannedCount":  result.get('ScannedCount', None),
+            "Count":  result.get('Count', None)
+        }
 
         return {
             "statusCode": 200,
-            "body": json.dumps(items),
+            "body": json.dumps(response),
             "headers": {
                 "Content-Type": "application/json",
                 "Access-Control-Allow-Origin": "*",
