760169c8bc0b92ed5347caeba68aab15e631d86f
Zied AYADI <zied.ayedi22@gmail.com>
2025-03-27 14:30:51 +0100

    Issue #83: Reviews Create dynamoDB table
    - Create dyname DB table
    - Using review_id (a unique string, like a UUID) as the range key ensures that multiple reviews can be stored for the same company_id. This is crucial because a company will naturally have many reviews.

--- terraform/dynamo-db/main.tf
+++ terraform/dynamo-db/main.tf
@@ -8,6 +8,50 @@ resource "aws_dynamodb_table" "companies" {
   }
 }
 
+resource "aws_dynamodb_table" "company_reviews" {
+  name           = "IT_VIBE_DEV_COMPANY_REVIEWS"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "company_id"
+  range_key      = "review_id" # To ensure uniqueness of reviews per company
+
+  attribute {
+    name = "company_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "review_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "rating"
+    type = "N" 
+  }
+
+  /**
+    * Global Secondary Index (GSI) to query reviews by rating
+    * it could be very useful if you wanted to get the reviews sorted by rating for a specific company, 
+    * or if you wanted to query for companies with a specific rating.
+    */
+  global_secondary_index {
+    name            = "RatingIndex"
+    hash_key        = "company_id"
+    range_key       = "rating"
+    projection_type = "ALL"
+  }
+
+  tags = {
+    Name        = "IT_VIBE_DEV_COMPANY_REVIEWS"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
+
   output "companies_table_arn" {
     value = aws_dynamodb_table.companies.arn
+  }
+
+  output "company_reviews_table_arn" {
+    value = aws_dynamodb_table.company_reviews.arn
   }
