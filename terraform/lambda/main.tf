62317f841f87232b53eac2ac8bf46d821d946532
Zied AYADI <zied.ayedi22@gmail.com>
2025-03-27 14:54:26 +0100

    Issue #83 Get reviews by company Id Lambda

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -67,11 +67,11 @@ data "archive_file" "get_company_review_metrics_lambda_code" {
   output_path = "../it-vibe-be/lambda/get-company-review-metrics/get_company_review_metrics.zip"
 }
 
-resource "aws_lambda_function" "get_comany_details_lambda" {
+resource "aws_lambda_function" "get_company_review_metrics_lambda" {
     filename         = data.archive_file.get_company_review_metrics_lambda_code.output_path
-    function_name    = "get_company_details"
+    function_name    = "get_company_review_metrics"
     role             = "arn:aws:iam::327441465709:role/DynamoDbReadWriteRole"
-    handler          = "get_company_details.lambda_handler"
+    handler          = "get_company_review_metrics.lambda_handler"
     runtime          = "python3.13"
     source_code_hash = data.archive_file.get_company_review_metrics_lambda_code.output_base64sha256
 
@@ -82,6 +82,27 @@ resource "aws_lambda_function" "get_comany_details_lambda" {
     }
 }
 
+# Reviews Lambda Functions
+data "archive_file" "get_reviews_by_company_id_lambda_code" {
+  type        = "zip"
+  source_file = "../it-vibe-be/lambda/get-reviews-by-company-id/get_reviews_by_company_id.py"
+  output_path = "../it-vibe-be/lambda/get-reviews-by-company-id/get_reviews_by_company_id.zip"
+}
+
+resource "aws_lambda_function" "get_reviews_by_company_id_lambda" {
+    filename         = data.archive_file.get_reviews_by_company_id_lambda_code.output_path
+    function_name    = "get_reviews_by_company_id"
+    role             = "arn:aws:iam::327441465709:role/DynamoDbReadWriteRole"
+    handler          = "get_reviews_by_company_id.lambda_handler"
+    runtime          = "python3.13"
+    source_code_hash = data.archive_file.get_reviews_by_company_id_lambda_code.output_base64sha256
+
+    environment {
+        variables = {
+            COMPANY_REVIEWS_TABLE_NAME = "IT_VIBE_DEV_COMPANY_REVIEWS"
+        }
+    }
+}
 
 data archive_file "push_contact_message_lamnda_code" {
   type        = "zip"
