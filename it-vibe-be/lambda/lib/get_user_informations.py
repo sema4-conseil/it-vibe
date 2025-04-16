842386a2ddf97999a274a36f2adc783574f3c969
Zied AYADI <zied.ayedi22@gmail.com>
2025-04-16 16:45:02 +0200

    Issue #45: Delete company Lambda
    - Add "isDeleted" to the company model when creating new company
    - Create lambda "delete_company.py" that delete the company from path_param
    - set "isDeleted" to true.
    - Add field: deleteDatetime
    - add field: deletedBy ""

---
+++ it-vibe-be/lambda/lib/get_user_informations.py
@@ -0,0 +1,14 @@
+def get_user_informations(event):
+    """
+        get user informations from cognito token
+    """
+    try:
+        # Extract the Cognito claims from the requestContext
+        claims = event['requestContext']['authorizer']['claims']
+        user_id = claims.get('sub', '')
+        email = claims.get('email', '')
+        groups = claims.get('cognito:groups', '')
+        return user_id, email, groups
+    except Exception as e:
+        print(e)
+        return None, None, None
