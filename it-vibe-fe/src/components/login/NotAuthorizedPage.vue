6ab4b1b76a054d26f6d9f50768e08b78b6d35647
Zied AYADI <zied.ayedi22@gmail.com>
2025-04-30 14:45:38 +0200

    Issue #128: Manage companies screen
    - Add jwt-decode package to decode jwt token if exists.
    - Add new empty component Manage companies
    - Make the route to manage-companies protected by guard, If the user is not admin it is redirected to Not authrized page.

---
+++ it-vibe-fe/src/components/login/NotAuthorizedPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Not Authorized</h1>
+    <h1>404</h1>
+    <p>You are not authorized to access this page.</p>
+    <a href="/">Go to Home</a>
+  </div>
+</template>
+<style scoped>
+h1 {
+  color: var(--error-red);
+}
+</style>
