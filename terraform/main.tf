aded3444e2b20e109f1331dae391a19e3ae00f1e
Zied AYADI <zied.ayedi22@gmail.com>
2025-05-06 15:28:42 +0200

    Issue #134: ContactPage Terraform
    - Make aliases for lambda "getContactMessages"
    - wire dev stage with dev alias for lambda getContactMessage
    - Enable cloudWatch for apiGateway
    - apiGateway: Make permission in single terraform block with foreach

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,22 @@ variable "hosted_zone_id" {
   default = "Z09606333IIM9GKOYXW0S"
 }
 
+variable "account_id" {
+  type = string
+  default = "327441465709"
+}
+
+variable "region" {
+  type = string
+  default = "eu-west-3"
+}
+
+variable "env" {
+  type = string
+  description = "The environment name"
+  default = "dev"
+}
+
 module "front-end" {
   source = "./front-end"
   hosted_zone_id = var.hosted_zone_id
@@ -29,6 +45,9 @@ module "dynamo-db" {
 }
 
 module "api-gateway" {
+  account_id = var.account_id
+  region = var.region
+  env = var.env
   source = "./api-gateway"
   get_companies_lambda_invoke_arn = module.lambda.get_companies_lambda_invoke_arn
   get_companies_lambda_arn = module.lambda.get_companies_lambda_arn
@@ -40,6 +59,8 @@ module "api-gateway" {
   delete_company_by_id_lambda_invoke_arn = module.lambda.delete_company_by_id_lambda_invoke_arn
   push_contact_message_lambda_arn = module.lambda.push_contact_message_lambda_arn
   push_contact_message_lambda_invoke_arn = module.lambda.push_contact_message_lambda_invoke_arn
+  get_contact_messages_lambda_invoke_arn = module.lambda.get_contact_messages_lambda_invoke_arn
+  get_contact_messages_lambda_arn = module.lambda.get_contact_messages_lambda_arn
   get_reviews_by_company_id_lambda_arn = module.lambda.get_reviews_by_company_id_lambda_arn
   get_reviews_by_company_id_lambda_invoke_arn = module.lambda.get_reviews_by_company_id_lambda_invoke_arn
   add_review_lambda_invoke_arn = module.lambda.add_review_lambda_invoke_arn
