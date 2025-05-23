56fac093532129e14ba970ca1e55094757f645c0
Zied AYADI <zied.ayedi22@gmail.com>
2025-05-23 14:37:32 +0200

    Issue #168: Patch company: Front-end
    - Create new component and call the API
    - Create Preflight
    - Fix problems in lambda
    
    Resolves #168

--- it-vibe-fe/src/components/company/ManageCompanies.vue
+++ it-vibe-fe/src/components/company/ManageCompanies.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <CreateCompany></CreateCompany>
+    <patch-company></patch-company>
   </div>
 </template>
 <script>
 import CreateCompany from "./CreateCompany.vue";
+import PatchCompany from "./PatchCompany.vue";
 export default {
   name: "ManageCompanies",
   components: {
     CreateCompany,
+    PatchCompany,
   },
 };
 </script>
