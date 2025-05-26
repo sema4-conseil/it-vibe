8f7d607f04fc9d0006c113444d58aacd0fdfc36f
Zied AYADI <zied.ayedi22@gmail.com>
2025-05-26 16:12:47 +0200

    Issue #166: (Back-end) Add more infos to the review
    - Lambda
    - api
    
    Resolves #166

--- it-vibe-be/lambda/lib/mappers/review_mapper.py
+++ it-vibe-be/lambda/lib/mappers/review_mapper.py
@@ -7,12 +7,15 @@ def map(review_object):
         "comment": review_object["comment"],
         "creationDate": review_object["creationDate"],
         "review_id": review_object["review_id"],
+        "contractType": review_object["contractType"],
+        "startDate": review_object["startDate"],
+        "endDate": review_object.get("endDate"),  # Use get to avoid KeyError if endDate is not present
         # Map the owner object only if the review is not anonymous, and exclude user_id
         "owner": {
                 "email": review_object["owner"]["email"],
                 "username": review_object["owner"]["username"],
             } if not review_object["isAnonymous"] else None,
-        }
+        },
         return review_vo
     except KeyError as e:
         # Handle missing keys in review_object
